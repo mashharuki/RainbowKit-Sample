9d00f1caa66515dd03238aea5b9e82a01016421e
Haruki Kondo <avp.104.106.107@ae.auone-net.jp>
2023-08-05 08:16:34 +0000

    設定を追加

--- my-app/src/pages/_app.tsx
+++ my-app/src/pages/_app.tsx
@@ -1,6 +1,57 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@rainbow-me/rainbowkit/styles.css";
 
+import {
+  connectorsForWallets,
+  RainbowKitProvider,
+} from "@rainbow-me/rainbowkit";
+import { createConfig, WagmiConfig, configureChains } from "wagmi";
+import { mainnet, polygon } from "wagmi/chains";
+import {
+  walletConnectWallet,
+  metaMaskWallet,
+} from "@rainbow-me/rainbowkit/wallets";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { AppProps } from "next/app";
+
+// get chains & publicClient data
+const { chains, publicClient } = configureChains(
+  [mainnet, polygon],
+  [
+    alchemyProvider({ 
+      apiKey: `${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}` 
+    }),
+    publicProvider(),
+  ]
+);
+
+// connecttosの設定
+const connectors = connectorsForWallets([
+  {
+    groupName: "Recommended",
+    wallets: [
+      walletConnectWallet({ chains }),
+      metaMaskWallet({ chains }),
+    ],
+  },
+]);
+
+// wagmi用の設定
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+/**
+ * App Component
+ */
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
